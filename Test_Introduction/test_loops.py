23686ea703dee7b9e9122bb0adc5abc2c446208a
Steven <kiralyste@gmail.com>
2017-06-10 00:32:03 +0200

    changed pytest testcase

--- Test_Introduction/test_loops.py
+++ Test_Introduction/test_loops.py
@@ -7,4 +7,14 @@ def test_loops_output(capsys):
     out = capsys.readouterr()
     assert out == "0\n1\n4\n9\n16\n"
 
+def test_myoutput(capsys): # or use "capfd" for fd-level
+    print ("hello")
+    sys.stderr.write("world\n")
+    out, err = capsys.readouterr()
+    assert out == "hello\n"
+    assert err == "world\n"
+    print ("next")
+    out, err = capsys.readouterr()
+    assert out == "next\n"
+
     #source https://docs.pytest.org/en/latest/capture.html#accessing-captured-output-from-a-test-function
