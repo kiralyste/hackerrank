5b6892368b5dd8140a35189086ee1355f528ad93
Steven <kiralyste@gmail.com>
2017-06-10 11:39:11 +0200

    added more testcases

--- Test_Introduction/test_printfunction.py
+++ Test_Introduction/test_printfunction.py
@@ -9,4 +9,23 @@ def test_printfunction(capsys):
     printfunction.print_integer_row(3)
     out, err = capsys.readouterr()
     assert out == "123"
-    
+       
+    printfunction.print_integer_row(5)
+    out, err = capsys.readouterr()
+    assert out == "12345"
+
+    printfunction.print_integer_row(10)
+    out, err = capsys.readouterr()
+    assert out == "12345678910"
+
+    printfunction.print_integer_row(20)
+    out, err = capsys.readouterr()
+    assert out == "1234567891011121314151617181920"
+
+    printfunction.print_integer_row(8)
+    out, err = capsys.readouterr()
+    assert out == "12345678"
+
+    printfunction.print_integer_row(4)
+    out, err = capsys.readouterr()
+    assert out == "1234"
