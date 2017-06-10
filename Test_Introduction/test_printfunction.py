2faa1586ceaa72763b77d50dc5c4f4b4d1d296b0
Steven <kiralyste@gmail.com>
2017-06-10 11:30:02 +0200

    modified testcases

--- Test_Introduction/test_printfunction.py
+++ Test_Introduction/test_printfunction.py
@@ -6,10 +6,6 @@ def test_printfunction():
     """
     Testcase for different small numbers
     """
-    assert printfunction.print_integer_row(3) == 123
-    assert printfunction.print_integer_row(5) == 12345
-    assert printfunction.print_integer_row(10) == 12345678910
-    assert printfunction.print_integer_row(20) == 1234567891011121314151617181920
-    assert printfunction.print_integer_row(8) == 12345678
-    assert printfunction.print_integer_row(4) == 1234
-    
+    printfunction.print_integer_row(3)
+    out, err = capsys.readouterr()
+    assert out == 123    
