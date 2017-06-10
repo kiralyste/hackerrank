65cdca17702d006a0f7965be6c3c9b4a5a0e9116
Steven <kiralyste@gmail.com>
2017-06-10 10:00:19 +0200

    changed testcase for leap_years

--- Test_Introduction/test_writeafunction.py
+++ Test_Introduction/test_writeafunction.py
@@ -3,13 +3,39 @@
 from Introduction import writeafunction
 
 def test_is_leap_year():
-    ''' Testcases for different Leap years and non-leap years '''
-    if writeafunction.is_leap(1990):
-        raise AssertionError
-    else:
-        pass
+    ''' Testcases for different Leap years '''
     assert writeafunction.is_leap(2016)
     assert writeafunction.is_leap(2020)
     assert writeafunction.is_leap(2024)
     assert writeafunction.is_leap(2028)
     assert writeafunction.is_leap(2032)
+
+def test_is_not_leap_year():
+    """
+     Testcases for different non-leap years
+    """
+    if writeafunction.is_leap(2019):
+        raise AssertionError
+    else:
+        pass
+    if writeafunction.is_leap(2021):
+        raise AssertionError
+    else:
+        pass
+    if writeafunction.is_leap(2022):
+        raise AssertionError
+    else:
+        pass
+    if writeafunction.is_leap(2023):
+        raise AssertionError
+    else:
+        pass
+    if writeafunction.is_leap(2025):
+        raise AssertionError
+    else:
+        pass
+    if writeafunction.is_leap(2026):
+        raise AssertionError
+    else:
+        pass
+    
