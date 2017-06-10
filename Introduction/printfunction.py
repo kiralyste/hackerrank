741270aed1f670759993607182a4eaf1f2a36d54
Steven <kiralyste@gmail.com>
2017-06-10 11:23:06 +0200

    removed input

--- Introduction/printfunction.py
+++ Introduction/printfunction.py
@@ -5,6 +5,5 @@ def print_integer_row(value_n):
     Print 123...n,
     ... represents the value in between
     """
-    value_n = int(input())
     for i in range(value_n):
         print(i+1, end='')
