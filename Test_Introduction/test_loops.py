1cac3784d6f0861d826f6a4bc2ed3bf0366f5704
Steven <kiralyste@gmail.com>
2017-06-10 09:13:10 +0200

    fixed testcase loop

--- Test_Introduction/test_loops.py
+++ Test_Introduction/test_loops.py
@@ -6,10 +6,5 @@ def test_loops_output(capsys):
     '''  testcase for 5 lines '''
     loops.loop(5)
     out, err = capsys.readouterr()
-    cmdout_line = """0\n
-                  1\n
-                  4\n
-                  9\n
-                  16\n"""
+    cmdout_line = "0\n" + "1\n" + "4\n" + "9\n" + "16\n"
     assert out == cmdout_line
-    
