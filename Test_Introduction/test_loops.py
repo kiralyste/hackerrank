cc7930992104cfc52034f26d58d174e925921838
Steven <kiralyste@gmail.com>
2017-06-10 08:39:36 +0200

    fixed loop testcase

--- Test_Introduction/test_loops.py
+++ Test_Introduction/test_loops.py
@@ -6,8 +6,8 @@ def test_loops_output(capsys):
     '''  testcase for 5 lines '''
     loops.loop(5)
     out, err = capsys.readouterr()
-    assert out == """0
-                     1
-                     4
-                     9
+    assert out == """0\n
+                     1\n
+                     4\n
+                     9\n
                      16\n"""
