7a727c59bb60ae6cd279ad1a6fb6ec264807a464
Steven <kiralyste@gmail.com>
2017-06-10 09:05:42 +0200

    changed loops testcase

--- Test_Introduction/test_loops.py
+++ Test_Introduction/test_loops.py
@@ -6,8 +6,10 @@ def test_loops_output(capsys):
     '''  testcase for 5 lines '''
     loops.loop(5)
     out, err = capsys.readouterr()
-    assert out ==   '0\n' +
-                    '1\n' +
-                    '4\n'
-                    '9\n' +
-                    '16\n'
+    cmdout_line = """0\n
+                  1\n
+                  4\n
+                  9\n
+                  16\n"""
+    assert out == cmdout_line
+    
